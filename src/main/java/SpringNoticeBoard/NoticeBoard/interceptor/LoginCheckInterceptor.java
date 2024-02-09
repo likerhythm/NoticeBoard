5ab42275448fe11c4d17998af96e19bc27753408
likerhythm <wjdalsdl0412@gmail.com>
2024-02-09 22:13:21 +0900

    fix: 게시물 작성 페이지로 redirect 되지 않던 문제 해결

--- src/main/java/SpringNoticeBoard/NoticeBoard/interceptor/LoginCheckInterceptor.java
+++ src/main/java/SpringNoticeBoard/NoticeBoard/interceptor/LoginCheckInterceptor.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 public class LoginCheckInterceptor implements HandlerInterceptor {
-
+    //TODO 서버에 존재하지 않는 페이지로 이동할 때도 로그인을 요구함.
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
@@ -19,17 +19,15 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         HttpSession session = request.getSession(false);
         log.info("[PREHANDLE]requestURI={}", requestURI);
 
-        if ("/post/save".equals(requestURI)) {
-            response.sendRedirect("/login?redirectURL=" + requestURI);
-            return false;
-        }
+
         //로그인 상태가 아닌 경우
         if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
-            if (!requestURI.startsWith("/post")) {
+            if (!requestURI.startsWith("/post") || "/post/save".equals(requestURI)) {
                 response.sendRedirect("/login?redirectURL=" + requestURI);
                 return false;
             }
         }
+
         String loginUserName;
         if (session != null) {
             loginUserName = ((User) session.getAttribute(SessionConst.LOGIN_USER)).getName();
